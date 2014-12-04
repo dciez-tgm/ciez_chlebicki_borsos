e252c49aad235dea2924ed5bfd67ffe8ff583fc0
David Ciez <dciez@student.tgm.ac.at>
2014-12-04 08:56:43 +0100

    adding ciez_chlebicki_borsos folder

---
+++ src/ciez/Zahlen.java
@@ -0,0 +1,25 @@
+package ciez;
+
+public class Zahlen {
+
+	
+	public Zahlen(){
+		
+	}
+	
+	public void add(){
+		
+	}
+	
+	public void summe(){
+		
+	}
+	
+	public void maximum(){
+		
+	}
+	
+	public void minimum(){
+		
+	}
+}
